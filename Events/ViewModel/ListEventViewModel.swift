a792fa12d0d5bb7637c2d4e3882abaf52db526d0
Márcio Habigzang Brufatto <mhbrufatto@gmail.com>
2020-04-22 01:46:58 -0300

    Criada a camada de view model para listar os eventos;

---
+++ Events/ViewModel/ListEventViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  ListEventViewModel.swift
+//  Events
+//
+//  Created by Marcio Habigzang Brufatto on 22/04/20.
+//  Copyright © 2020 Marcio Habigzang Brufatto. All rights reserved.
+//
+
+import Foundation
+
+class ListEventViewModel: ListEventViewModelProtocol {
+    
+    private var events: [Event] = []
+    private var networkManagerProtocol: NetworkManagerProtocol
+    
+    init(networkManagerProtocol: NetworkManagerProtocol) {
+        self.networkManagerProtocol = networkManagerProtocol
+        self.networkManagerProtocol.getEvents(completion: { (events) in
+            self.events = events
+        })
+    }
+    
+    func numberOfRows() -> Int {
+        return self.events.count
+    }
+    
+    func eventAt(_ index: Int) -> Event {
+        return self.events[index]
+    }
+    
+    func loadEvents(completion: @escaping GetEventsClosure) {
+        self.networkManagerProtocol.getEvents(completion: { (events) in
+            self.updateListEvent(events: events)
+            completion(self.events)
+        })
+    }
+    
+    func updateListEvent(events: [Event]) {
+        for event in events {
+            if !self.events.contains(where: { $0.id == event.id }) {
+                self.events.append(event)
+            }
+        }
+    }
+}
